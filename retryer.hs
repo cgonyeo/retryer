ea25bbb2f165ec456d79535eb68e45318f0290b1
Robert Glossop <robgssp@gmail.com>
2014-12-15 21:44:59 -0500

    Optional flags, also don't delay between last command and exit

--- retryer.hs
+++ retryer.hs
@@ -4,26 +4,29 @@ import Options.Applicative
 import Data.Monoid
 import System.Exit
 import Control.Concurrent
+import Data.Maybe
 
 data Flags = Flags { tries :: Int
                    , delay :: Double
                    , command :: [String]
                    } deriving(Show,Eq)
 
+optionalV v p = fromMaybe v <$> optional p
+
 flgparse :: Parser Flags
 flgparse = Flags
-    <$> option auto
+    <$> (optionalV 1 $ option auto
         (  long "tries"
         <> short 't'
         <> metavar "<int>"
         <> help "Number of times to retry the command. <= 0 disables."
-        )
-    <*> option auto
+        ))
+    <*> (optionalV 1 $ option auto
         (  long "delay"
         <> short 'd'
         <> metavar "<double>"
         <> help "Seconds to wait before retrying the command. <= 0 disables."
-        )
+        ))
     <*> some (argument str (metavar "-- Command"))
 
 main :: IO ()
@@ -35,7 +38,7 @@ run (Flags t d (c:a)) = do
         exit <- waitForProcess =<< (runCommand $ foldl ((++).(++ " ")) c a)
         case exit of
             ExitSuccess   -> exitSuccess
-            ExitFailure _ -> do threadDelay (truncate $ 10^6 * d)
-                                if t == 1
-                                    then exitFailure
-                                    else run (Flags (t-1) d (c:a))
+            ExitFailure _ -> if t == 1
+                                then exitFailure
+                                else do threadDelay (truncate $ 10^6 * d)
+                                        run (Flags (t-1) d (c:a))
